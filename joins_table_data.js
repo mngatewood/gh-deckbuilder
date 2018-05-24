9e621ec96eb4232fc88c6fc51fe77d5deabaa787
Michael Gatewood <michael@mngatewood.com>
2018-05-24 14:22:44 -0600

    Adds data sources for tables

---
+++ joins_table_data.js
@@ -0,0 +1,82 @@
+export const joinsData = [
+  {
+    Card_id: 1,
+    Deck_id: 1
+  },
+  {
+    Card_id: 2,
+    Deck_id: 1
+  },
+  {
+    Card_id: 3,
+    Deck_id: 1
+  },
+  {
+    Card_id: 4,
+    Deck_id: 1
+  },
+  {
+    Card_id: 5,
+    Deck_id: 1
+  },
+  {
+    Card_id: 6,
+    Deck_id: 1
+  },
+  {
+    Card_id: 7,
+    Deck_id: 1
+  },
+  {
+    Card_id: 8,
+    Deck_id: 1
+  },
+  {
+    Card_id: 9,
+    Deck_id: 1
+  },
+  {
+    Card_id: 10,
+    Deck_id: 1
+  },
+  {
+    Card_id: 31,
+    Deck_id: 2
+  },
+  {
+    Card_id: 32,
+    Deck_id: 2
+  },
+  {
+    Card_id: 33,
+    Deck_id: 2
+  },
+  {
+    Card_id: 34,
+    Deck_id: 2
+  },
+  {
+    Card_id: 35,
+    Deck_id: 2
+  },
+  {
+    Card_id: 36,
+    Deck_id: 2
+  },
+  {
+    Card_id: 37,
+    Deck_id: 2
+  },
+  {
+    Card_id: 38,
+    Deck_id: 2
+  },
+  {
+    Card_id: 39,
+    Deck_id: 2
+  },
+  {
+    Card_id: 40,
+    Deck_id: 2
+  },
+]
