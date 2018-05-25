48811cfd5dacf11e928145ac8926f38cbe646d92
Michael Gatewood <michael@mngatewood.com>
2018-05-24 18:58:36 -0600

    Adds test env and seed

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,30 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/deckbuilder_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/prod'
+    },
+    useNullAsDefault: true
   }
 
 };
