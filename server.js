1ccd72dd4eebe9c83aad98d9d8cf6ade01b76ab7
Michael Gatewood <michael@mngatewood.com>
2018-05-25 15:44:26 -0600

    Adds get deck and associated cards

--- server.js
+++ server.js
@@ -30,6 +30,35 @@ app.get('/api/v1/cards/:class', (request, response) => {
     .json('Internal server error ' + error));
 })
 
+app.get('/api/v1/decks/:id', (request, response) => {
+  const deckId = request.params.id;
+
+  database('decks').where('id', deckId).select()
+    .then( deck => {
+      if(deck.length) {
+        database('joins').where('deck_id', deckId).select()
+        .then(cards => {
+          if(cards.length) {
+            let cardsArray = [];
+            cards.forEach(card => {
+              cardsArray.push(card.card_id)
+            })
+            return response.status(200).json({
+              name: deck[0].name,
+              cards: cardsArray
+            })
+          } else {
+            return response.status(404).json('No matching cards found.');
+          }
+        })
+      } else {
+        return response.status(404).json('No matching decks found.');
+      }
+    })
+    .catch(error => response.status(500)
+      .json('Internal server error ' + error));
+})
+
 app.post('/api/v1/decks', (request, response) => {
 
   const deck = request.body;
