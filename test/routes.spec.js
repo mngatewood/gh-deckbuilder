150c794a4b9a11cc58ec2c9376a64c1cced25334
Kailin Cannon <kailin.cannon@gmail.com>
2018-05-26 00:30:50 -0600

    First test done

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,7 +5,7 @@ const { app, database } = require('../server');
 
 chai.use(chaiHttp);
 
-describe('client routes', () => {
+describe.skip('client routes', () => {
   it('should return the homepage', (done) => {
     chai.request(app)
       .get('/')
@@ -35,7 +35,7 @@ describe('API Routes', () => {
         });
       });
     });
-});
+  });
 
   it.skip('should GET all the cards', (done) => {
     chai.request(app)
@@ -58,23 +58,25 @@ describe('API Routes', () => {
   })
 
   describe("GET cards by :class", () => {
-    chai.request(app)
-      .get('/api/v1/cards:mindthief')
-      .end((error, response) => {
-        response.should.be.json
-        response.should.have.status(200)
-        response.body.should.be.an('array')
-        response.body.length.should.equal(174)
-        // response.body[0].should.have.property('id', 1);
-        // response.body[0].should.have.property('class', 'Brute');
-        // response.body[0].should.have.property('name');
-        // response.body[0].should.have.property('initiative');
-        // response.body[0].should.have.property('top_action');
-        // response.body[0].should.have.property('bottom_action');
-        // response.body[0].should.have.property('image_url');
-        // response.body[0].should.have.property('card_level', 1);
-        done()
-      })
+    it("should GET all cards of a specified class", (done) => {
+      chai.request(app)
+        .get('/api/v1/cards/Mindthief')
+        .end((error, response) => {
+          response.should.be.json
+          response.should.have.status(200);
+          response.body.should.be.an('array');
+          response.body.length.should.equal(29);
+          // response.body[0].should.have.property('id', 66);
+          response.body[0].should.have.property('class', 'Mindthief');
+          response.body[0].should.have.property('name');
+          response.body[0].should.have.property('initiative');
+          response.body[0].should.have.property('top_action');
+          response.body[0].should.have.property('bottom_action');
+          response.body[0].should.have.property('image_url');
+          response.body[0].should.have.property('card_level', 1);
+          done();
+        })
+    });
   });
 
 });
