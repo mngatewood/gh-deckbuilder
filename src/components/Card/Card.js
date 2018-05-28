c904d3b29a263d854a3c56d90b1437b2a3bd007d
Michael Gatewood <michael@mngatewood.com>
2018-05-27 23:28:05 -0600

    Adds drag and drop funcationality (DOM only)

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,10 +2,19 @@ import './Card.css';
 import React from 'react';
 
 export const Card = (card) => {
-  
+
+  const dragstartHandler = (event) => {
+    console.log(event.target);
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.dataTransfer.dropEffect = "move";
+  }
+
   return (
-    <div className="card">
-      <img src={card.image} alt={card.name}/>
+    <div className="card" 
+      id={card.id}
+      style={{ backgroundImage: `url(${card.image})`}}
+      draggable="true"
+      onDragStart={ (event) => dragstartHandler(event) }>
     </div>
   )
 
