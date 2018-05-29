654f0e21688ef2e2fd527271c81b96aacc486dad
Charles Yach <CharlesY712@gmail.com>
2018-05-29 08:24:08 -0600

    Adding a few more images and tweaking welcome.css

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -7,14 +7,14 @@ export const Welcome = () => {
 
   return (
     <div className="full-page">
-    <section className="class-select-container">
-      <NavLink to="/Brute" className="brute"></NavLink>
-      <NavLink to="/Cragheart" className="cragheart"></NavLink>
-      <NavLink to="/Mindthief" className="mindthief"></NavLink>
-      <NavLink to="/Scoundrel" className="scoundrel"></NavLink>
-      <NavLink to="/Spellweaver" className="spellweaver"></NavLink>
-      <NavLink to="/Tinkerer" className="tinkerer"></NavLink>
-    </section>
+      <section className="class-select-container">
+        <NavLink to="/Brute" className="brute"></NavLink>
+        <NavLink to="/Cragheart" className="cragheart"></NavLink>
+        <NavLink to="/Mindthief" className="mindthief"></NavLink>
+        <NavLink to="/Scoundrel" className="scoundrel"></NavLink>
+        <NavLink to="/Spellweaver" className="spellweaver"></NavLink>
+        <NavLink to="/Tinkerer" className="tinkerer"></NavLink>
+      </section>
     </div>
   );
 };
