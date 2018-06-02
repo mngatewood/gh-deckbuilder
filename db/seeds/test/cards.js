6b124e048a46eec263f9d594ee41932ddd75625e
Michael Gatewood <michael@mngatewood.com>
2018-06-02 17:13:31 -0600

    Adds decks.level to migration, seeds, and server.js

--- db/seeds/test/cards.js
+++ db/seeds/test/cards.js
@@ -29,9 +29,9 @@ exports.seed = (knex, Promise) => database.migrate.rollback()
   // add two records to decks table
   .then(() => {
     return Promise.all([
-      knex('decks').insert({ name: 'Brute Deck', class: 'Brute' }),
-      knex('decks').insert({ name: 'Cragheart Deck', class: 'Cragheart' }),
-      knex('decks').insert({ name: 'Mindthief Deck', class: 'Mindthief' })
+      knex('decks').insert({ name: 'Brute Deck', class: 'Brute', level: 1 }),
+      knex('decks').insert({ name: 'Cragheart Deck', class: 'Cragheart', level: 2 }),
+      knex('decks').insert({ name: 'Mindthief Deck', class: 'Mindthief', level: 9 })
     ]);
   })
 
