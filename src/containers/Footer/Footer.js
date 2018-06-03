6484fa6e4dc3cc1d5051c9f2ce73779b685a097b
Charles Yach <CharlesY712@gmail.com>
2018-06-03 16:32:26 -0600

    Working out dynamicIcon

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 // import { withRouter } from 'react-router';
 import * as api from '../../api/index';
+import * as helpers from '../../helpers/index';
+import * as icons from '../../images/index'
 import './Footer.css';
 
 export class Footer extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      decks: [],
+      error: ''
+    };
+  };
 
   async componentDidMount() {
     try {
       const decks = await api.fetchDecks();
-      console.log(decks);
-      
+      this.setState({ decks })
     } catch (error) {
       this.setState({ error })
     }
   }
 
+  mapDecks = (decks) => {
+    decks.map(deck => {
+      const dynamicIcon = `../../images/classIcons/${deck.class}Icon.png`
+      
+      // require(`../../images/classIcons/${deck.class}Icon.png`)
+      console.log(dynamicIcon)
+      return <img src={dynamicIcon} alt={deck.name}/>
+    })
+  }
 
   render() {
     return (
@@ -25,7 +41,10 @@ export class Footer extends Component {
           <h1>SAVED DECKS</h1>
         </div>
         <div>
-
+          {
+            this.state.decks.length &&
+            this.mapDecks(this.state.decks)
+          }
         </div>
       </footer>
     );
