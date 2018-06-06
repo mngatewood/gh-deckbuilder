53d64ea92dc0cd23a0a313071fe9c5601accdf9b
Charles Yach <CharlesY712@gmail.com>
2018-06-06 04:55:01 -0600

    jsx

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
+  const titleImg = require('../../images/cropped-gloomhaven-logo.png');
+
   return (
     <header>
-      <h1>GLOOMHEAVEN</h1>
+      <Link to="/">
+        <img className="header-title-image" src={titleImg} alt="title"/>
+      </Link>
     </header>
   );
 };
