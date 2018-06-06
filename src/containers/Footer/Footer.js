d4d3581f6cacf5af0683da10cb770a612035b181
Charles Yach <CharlesY712@gmail.com>
2018-06-05 19:19:23 -0600

    Fixed config with API key

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -7,15 +7,14 @@ import FirebaseAuth from 'react-firebaseui/FirebaseAuth';
 import firebase from 'firebase';
 import './Footer.css';
 
-const config = {
-  apiKey: "AIzaSyBHV499x0Il6LN4XYA1JCExxTH_kgkg3pg",
-  authDomain: "gh-deckbuilder.firebaseapp.com",
-  databaseURL: "https://gh-deckbuilder.firebaseio.com",
-  projectId: "gh-deckbuilder",
-  storageBucket: "gh-deckbuilder.appspot.com",
-  messagingSenderId: "997988713770"
-};
-firebase.initializeApp(config);
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+});
 
 export class Footer extends Component {
   constructor(props) {
